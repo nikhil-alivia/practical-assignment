879b08c22640a29abea42b7c7fbb29421e7b3a31
nikhil-alivia <nikhil.jugdan@aliviaanalytics.com>
2025-06-29 21:22:28 +0530

    Add custom api exception

--- auth-service/src/main/java/dev/nikhilj/authservice/exceptions/GlobalExceptionHandler.java
+++ auth-service/src/main/java/dev/nikhilj/authservice/exceptions/GlobalExceptionHandler.java
@@ -18,8 +18,8 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public ResponseEntity<Map<String, String>> handleValidationExceptions(
-		MethodArgumentNotValidException ex
-	){
+			MethodArgumentNotValidException ex
+	) {
 		Map<String, String> errors = new HashMap<>();
 		ex.getBindingResult().getFieldErrors().forEach(
 				error -> errors.put(
@@ -32,9 +32,17 @@ public class GlobalExceptionHandler {
 
 	@ExceptionHandler(HttpMessageNotReadableException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public ResponseEntity<Map<String, String>> handleMissingRequestBody() {
+	public ResponseEntity<Map<String, String>> handleMissingRequestBody(HttpMessageNotReadableException ex) {
 		Map<String, String> errors = new HashMap<>();
 		errors.put("error", "Request body is missing or malformed");
 		return ResponseEntity.badRequest().body(errors);
 	}
+
+	@ExceptionHandler(APIException.class)
+	public ResponseEntity<Map<String, String>> handleAPIException(APIException ex) {
+		Map<String, String> errors = new HashMap<>();
+		errors.put("error", ex.getMessage());
+		return new ResponseEntity<>(errors, ex.getStatus());
+	}
+
 }
