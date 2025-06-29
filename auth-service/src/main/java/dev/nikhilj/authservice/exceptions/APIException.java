2252a92e94b7244c9899b824afc432a626de2a1c
nikhil-alivia <nikhil.jugdan@aliviaanalytics.com>
2025-06-29 21:22:18 +0530

    Add custom api exception

---
+++ auth-service/src/main/java/dev/nikhilj/authservice/exceptions/APIException.java
@@ -0,0 +1,12 @@
+package dev.nikhilj.authservice.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@Getter
+public class APIException extends RuntimeException {
+	private HttpStatus status;
+	private String message;
+}
