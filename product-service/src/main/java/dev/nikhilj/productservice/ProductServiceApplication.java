d9a2c08b689d488bea809bb1a6043c4690a075de
nikhil-alivia <nikhil.jugdan@aliviaanalytics.com>
2025-06-30 19:03:21 +0530

    Add new product service

---
+++ product-service/src/main/java/dev/nikhilj/productservice/ProductServiceApplication.java
@@ -0,0 +1,16 @@
+package dev.nikhilj.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {
+		"dev.nikhilj.productservice",
+		"dev.nikhilj.common"
+})
+public class ProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceApplication.class, args);
+	}
+
+}
---
+++ product-service/src/test/java/dev/nikhilj/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nikhilj.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
