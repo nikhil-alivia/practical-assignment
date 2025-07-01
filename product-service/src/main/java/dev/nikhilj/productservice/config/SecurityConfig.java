4c244a8e5b8fd0d566c4341c5080c38e842569c7
nikhil-alivia <nikhil.jugdan@aliviaanalytics.com>
2025-07-01 14:44:43 +0530

    Configure Auth for product service

---
+++ product-service/src/main/java/dev/nikhilj/productservice/config/SecurityConfig.java
@@ -0,0 +1,77 @@
+package dev.nikhilj.productservice.config;
+
+import dev.nikhilj.common.security.utils.JWTAuthenticationFilter;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+@EnableMethodSecurity
+@AllArgsConstructor
+public class SecurityConfig {
+	private AuthenticationEntryPoint authenticationEntryPoint;
+	private JWTAuthenticationFilter authenticationFilter;
+
+	@Bean
+	public static PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public static AuthenticationManager authenticationManager(
+			AuthenticationConfiguration authenticationConfiguration
+	) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration corsConfig = new CorsConfiguration();
+		corsConfig.setAllowedOrigins(List.of("*"));
+		corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+		corsConfig.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Auth-Token"));
+		corsConfig.setAllowCredentials(true);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfig);
+		return source;
+	}
+
+	@Bean
+	SecurityFilterChain securityFilterChain(
+			HttpSecurity httpSecurity
+	) throws Exception {
+		httpSecurity.csrf(AbstractHttpConfigurer::disable)
+				.authorizeHttpRequests(
+						authorize -> authorize
+								.requestMatchers("GET","/api/**").permitAll()
+								.anyRequest().authenticated()
+				)
+				.exceptionHandling(exc -> exc.authenticationEntryPoint(authenticationEntryPoint))
+				.sessionManagement(
+						session -> session.sessionCreationPolicy(
+								SessionCreationPolicy.STATELESS
+						)
+				);
+		httpSecurity.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+		return httpSecurity.build();
+	}
+
+
+}
